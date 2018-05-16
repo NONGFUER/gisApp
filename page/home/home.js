88164f1f0bd75c301c7865059e2e4ec3a227cc91
驱蚊小将 <13852291705@163.com>
2018-05-16 08:40:48 +0800

    modify swiper style

--- page/home/home.js
+++ page/home/home.js
@@ -1,18 +1,18 @@
 $(function(){
  
   	var mySwiper = new Swiper ('.swiper-container', {
-	   	effect: 'coverflow',
+	   //	effect: 'coverflow',
 	   	loop:true,
 	    grabCursor: true,
 	    centeredSlides: true,
 	    slidesPerView: 'auto',
-	    coverflowEffect: {
-	       rotate: 50,
-	       stretch: 0,
-	       depth: 100,
-	       modifier: 1,
-	       slideShadows : true,
-	    },
+	    // coverflowEffect: {
+	    //    rotate: 50,
+	    //    stretch: 0,
+	    //    depth: 100,
+	    //    modifier: 1,
+	    //    slideShadows : true,
+	    // },
 	    pagination: {
 	       el: '.swiper-pagination',
 	    },
